f4156c2bff55e5ba9ecff3797859cc5e932439f8
M.Furkan Uygur <76523870+mfurkanuygur@users.noreply.github.com>
2024-07-03 16:20:35 +0300

    todo app comment delete

--- src/components/single-todo/single-todo.component.ts
+++ src/components/single-todo/single-todo.component.ts
@@ -16,13 +16,12 @@ export class SingleTodoComponent implements OnInit {
   constructor(private todosService: TodosService) {}
 
   ngOnInit(): void {
-    this.todosService.getItems(); // İlk yüklemede verileri al
+    this.todosService.getItems();
     this.todosService.allTodosChange.subscribe((updatedTodos) => {
-      this.allTodos = updatedTodos; // Olay tetiklendiğinde güncelle
+      this.allTodos = updatedTodos;
     });
   }
-  // constructor(private todosService: TodosService) {}
-  // allTodos = this.todosService.getItems();
+
   editTodo = (id: number) => {
     this.isEdit = !this.isEdit;
     this.updateTodoName = this.allTodos.find((t) => (t.id == id)).name;
@@ -33,7 +32,7 @@ export class SingleTodoComponent implements OnInit {
 
     if (this.updateTodoName.trim() !== '') {
       this.todosService.updateTodo(todoID, this.updateTodoName);
-      this.updateTodoName = ''; // Input değerini temizle
+      this.updateTodoName = '';
       this.isEdit = !this.isEdit;
     }
   };
@@ -45,50 +44,3 @@ export class SingleTodoComponent implements OnInit {
     this.todosService.deleteItem(id);
   };
 }
-/*
-import { Component, OnInit } from '@angular/core';
-import { TodosService } from '../../services/todos.service';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-@Component({
-  selector: 'app-single-todo',
-  standalone: true,
-  imports: [CommonModule,FormsModule],
-  templateUrl: './single-todo.component.html',
-  styleUrl: './single-todo.component.css',
-})
-export class SingleTodoComponent implements OnInit {
-  allTodos: any[] = [];
-  // isEdit: boolean = false;
-  updateTodoName: string = '';
-  constructor(private todosService: TodosService) {}
-
-  ngOnInit(): void {
-    this.todosService.getItems(); // İlk yüklemede verileri al
-    this.todosService.allTodosChange.subscribe((updatedTodos) => {
-      this.allTodos = updatedTodos; // Olay tetiklendiğinde güncelle
-    });
-  }
-  // constructor(private todosService: TodosService) {}
-  // allTodos = this.todosService.getItems();
-  editTodo = (id: number) => {
-    this.todosService.editTodo(id);
-  };
-  saveTodo = (e: Event,todoID:number) => {
-    e.preventDefault();
-
-    // Örneğin updateTodo fonksiyonu olduğunu varsayalım
-    this.todosService.updateTodo(todoID, this.updateTodoName);
-
-    this.updateTodoName = ''; // Input değerini temizle
-    
-  };
-
-  completeTodo = (id: number) => {
-    this.todosService.completeTodo(id);
-  };
-  deleteTodo = (id: number) => {
-    this.todosService.deleteItem(id);
-  };
-}
- */
